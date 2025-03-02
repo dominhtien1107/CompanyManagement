d904e15097472bd9ff8ca09103128147d9a503c2
minhtien <dominhtien199898@gmail.com>
2025-03-02 13:38:04 +0700

    Init project and config services, middlewares

---
+++ sources/CompanyManagementAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace CompanyManagementAPI.Extensions;
+
+// 1.4 Extension methods and CORS (Cross-Origin Resource Sharing) configuration.
+
+// Cơ chế cấp hoặc hạn chế quyền truy cập vào ứng dụng từ các "Domain" khác nhau.
+// Nếu muốn gửi Request từ một Domain khác vào ứng dụng => Cấu hình này là bắt buộc.
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy", builder =>
+        builder.AllowAnyOrigin() // Cho phép truy cập từ tất cả các nguồn <=> WithOrigins(): Chỉ định một nguồn cụ thể.
+        .AllowAnyMethod() // Cho phép tất cả các phương thức HTTP <=> WithMethods(): Chỉ định các phương thức cụ thể.
+        .AllowAnyHeader()); // <=> WithHeaders(): Chỉ định các tiêu đề cụ thể.
+    });
+
+    // 1.5 IIS Configuration
+    // Cấu hình tích hợp IIS
+    public static void ConfigureIISIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        { 
+            // Thêm các thuộc tính tại đây.
+            // Hiện tại chưa sử dụng đến.
+
+            //options.AutomaticAuthentication = true;
+            //options.AuthenticationDisplayName = "";
+            //options.ForwardClientCertificate = true;
+        });
+}
---
+++ sources/CompanyManagementAPI/Program.cs
@@ -0,0 +1,52 @@
+﻿using CompanyManagementAPI.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Thêm CORS và IIS vào phần cấu hình dịch vụ.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+builder.Services.AddControllers(); // Đăng ký các Controllers trong IServiceCollection (Không sử dụng Views).
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Tạo biến ứng dụng của lớp WebApplication.
+// Triển khai nhiều giao diện như: IHost, IApplicationBuilder, IEndpointRouteBuilder, ...
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+}
+else
+{
+    app.UseHsts(); // HTTP Strict Transport Security: tính năng bảo mật cho website. Đảm bảo rằng tất cả kết nối tới một Web brower được mã hóa bằng giao thức HTTPS.
+}
+
+// Thứ tự thêm các Middlewares là RẤT QUAN TRỌNG.
+
+app.UseHttpsRedirection(); // Chuyển hướng HTTP => HTTPS.
+
+app.UseStaticFiles(); // Cho phép sử dụng Static Files trong ứng dụng (Default: wwwroot).
+
+// Chuyển tiếp các tiêu đề Proxy => Request hiện tại.
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All 
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthorization(); // Uỷ quyền IApplicationBuilder đã chỉ định để kích hoạt khả năng ủy quyền. Phải nằm giữa app.UseRouting() và app.UseEndpoints(...) nếu cấp quyền.
+
+app.MapControllers(); // Thêm các điểm cuối từ Controller Actions vào IEndpointRouteBuilder.
+
+app.Run(); // Chạy ứng dụng và chặn luồng.
