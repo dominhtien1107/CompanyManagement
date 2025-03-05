52d5e06e2c7277b79f2a5f0f1b2fb9ae0c5216bc
minhtien <dominhtien199898@gmail.com>
2025-03-05 22:45:10 +0700

    Using ef core code first generate database

---
+++ sources/CompanyManagementAPI/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CompanyManagementAPI.ContextFactory;
+
+public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+{
+    public RepositoryContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+        var builder = new DbContextOptionsBuilder<RepositoryContext>().UseNpgsql(configuration.GetConnectionString("sqlConnection"), b => b.MigrationsAssembly("CompanyManagementAPI"));
+
+        //Return a new instance of our RepositoryContext class with provided options.
+        return new RepositoryContext(builder.Options);
+    }
+}
