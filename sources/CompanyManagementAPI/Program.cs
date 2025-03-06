d995ae278fb3dc2f10c01d4741b71ddb443bea84
minhtien <dominhtien199898@gmail.com>
2025-03-06 22:21:37 +0700

    Add repository pattern logic, add a service layer

--- sources/CompanyManagementAPI/Program.cs
+++ sources/CompanyManagementAPI/Program.cs
@@ -14,6 +14,9 @@ LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nl
 builder.Services.ConfigureCors();
 builder.Services.ConfigureIISIntegration();
 builder.Services.ConfigureLoggerService();
+builder.Services.ConfigureRepositoryManager();
+builder.Services.ConfigureServiceManager();
+builder.Services.ConfigureSqlContext(builder.Configuration);
 
 builder.Services.AddControllers(); // Đăng ký các Controllers trong IServiceCollection (Không sử dụng Views).
 
