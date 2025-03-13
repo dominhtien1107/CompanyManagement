4a1a054149e3a1eae5ea5106ba53e7c4562f64ac
minhtien <dominhtien199898@gmail.com>
2025-03-13 21:24:31 +0700

    Using PUT request with EF Core

--- sources/Service/CompanyService.cs
+++ sources/Service/CompanyService.cs
@@ -77,4 +77,13 @@ internal sealed class CompanyService : ICompanyService
             throw new CompanyNotFoundException(companyId);
         _repository.Company.DeleteCompany(company); _repository.Save();
     }
+
+    public void UpdateCompany(Guid companyId, CompanyForUpdateDto companyForUpdate, bool trackChanges) 
+    {
+        var companyEntity = _repository.Company.GetCompany(companyId, trackChanges);
+        if (companyEntity is null)
+            throw new CompanyNotFoundException(companyId);
+        _mapper.Map(companyForUpdate, companyEntity);
+        _repository.Save();
+    }
 }
